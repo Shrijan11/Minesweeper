45c50a3d928a7c8519bce420361560bb41f9dd7a
Shrijan11 <shrijanshrestha1@gmail.com>
2023-09-11 22:54:28 +0545

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -25,6 +25,25 @@ elapsed_time = 0
 score = 0
 bonus_score = 0
 
+# High scores
+high_scores = []
+
+# Function to load high scores from a file
+def load_high_scores():
+    try:
+        with open("high_scores.txt", "r") as file:
+            high_scores.extend(file.read().splitlines())
+    except FileNotFoundError:
+        pass
+
+# Function to save high scores to a file
+def save_high_scores():
+    with open("high_scores.txt", "w") as file:
+        file.write("\n".join(high_scores))
+
+# Load high scores at the start of the game
+load_high_scores()
+
 # Function to generate mines based on difficulty
 def generate_mines(difficulty):
     global mines, mine_count
@@ -48,7 +67,8 @@ def reveal(x, y):
         return
     revealed[y][x] = True
     if (x, y) in mines:
-        canvas.create_rectangle(x*30, y*30, (x+1)*30, (y+1)*30, fill="red")
+        # Animate the mine cell briefly
+        animate_mine(x, y)
         game_over()
     else:
         count = count_adjacent_mines(x, y)
@@ -73,7 +93,28 @@ def reveal(x, y):
         # Update the score label
         update_score_label()
 
+def animate_mine(x, y):
+    # Animate the color change of the clicked mine and then continue with the rest of the mines
+    mine_list = list(mines)
+    mine_list.remove((x, y))  # Remove the clicked mine from the list
+    canvas.create_rectangle(x*30, y*30, (x+1)*30, (y+1)*30, fill="red")
+    root.update()
+    time.sleep(0.2)
+    canvas.create_rectangle(x*30, y*30, (x+1)*30, (y+1)*30, fill="black")
+    root.update()
+    
+    for (mine_x, mine_y) in mine_list:
+        if not revealed[mine_y][mine_x]:
+            canvas.create_rectangle(mine_x*30, mine_y*30, (mine_x+1)*30, (mine_y+1)*30, fill="red")
+            root.update()
+            time.sleep(0.2)
+            canvas.create_rectangle(mine_x*30, mine_y*30, (mine_x+1)*30, (mine_y+1)*30, fill="black")
+            root.update()
+
+
+
 def game_over():
+    global high_scores
     for x in range(WIDTH):
         for y in range(HEIGHT):
             if (x, y) in mines:
@@ -81,6 +122,14 @@ def game_over():
     elapsed_seconds = round(elapsed_time, 2)
     if bonus_score == 200:
         canvas.create_text(WIDTH*15, HEIGHT*15, text="You Win!", font=("Helvetica", 24))
+        # Save the score in the high scores list
+        high_scores.append("Score: {} - Time: {} seconds".format(score + bonus_score, elapsed_seconds))
+        # Sort high scores in descending order
+        high_scores.sort(reverse=True, key=lambda x: int(x.split()[1]))
+        # Limit to 5 high scores
+        high_scores = high_scores[:5]
+        # Save high scores to a file
+        save_high_scores()
     else:
         canvas.create_text(WIDTH*15, HEIGHT*15, text="Game Over!", font=("Helvetica", 24))
     canvas.create_text(WIDTH*15, HEIGHT*45, text="Time: {} seconds".format(elapsed_seconds), font=("Helvetica", 16))
@@ -123,11 +172,25 @@ def start_game(difficulty):
     bonus_score = 0
     update_score_label()
 
+# Function to open the high scores window
+def open_high_scores_window():
+    high_scores_window = tk.Toplevel(root)
+    high_scores_window.title("High Scores")
+    high_scores_label = tk.Label(high_scores_window, text="High Scores", font=("Helvetica", 18))
+    high_scores_label.pack()
+    for i, high_score in enumerate(high_scores):
+        score_label = tk.Label(high_scores_window, text=f"{i + 1}. {high_score}", font=("Helvetica", 14))
+        score_label.pack()
+
 # Create buttons for selecting difficulty
 easy_button = tk.Button(root, text="Easy", command=lambda: start_game("Easy"))
 easy_button.pack(pady=10)
 hard_button = tk.Button(root, text="Hard", command=lambda: start_game("Hard"))
 hard_button.pack(pady=10)
+
+# Button to view high scores
+high_scores_button = tk.Button(root, text="High Scores", command=open_high_scores_window)
+high_scores_button.pack(pady=10)
 
 canvas = tk.Canvas(root, width=WIDTH*30, height=HEIGHT*30)
 canvas.bind("<Button-1>", left_click)
